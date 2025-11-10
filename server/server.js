80879d2985b76caa0f511b0772482b70624e57e3
Mordenkian <jasonwang7041@gmail.com>
2025-11-09 16:33:51 -0800

    partial update for database storage of saved lcoations for weather data. Not working.

--- server/server.js
+++ server/server.js
@@ -4,11 +4,28 @@ const mysql = require('mysql2');
 const mongoose = require('mongoose');
 const { MongoError } = require('mongodb');
 
-mongoose.connect('mongodb://localhost:27017/companyDB');
+mongoose.connect('mongodb+srv://jwang7041:7041Nestings%25@cluster0.cmuuf0a.mongodb.net/SavedLocations');
 
 const ProjectSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    budget: { type: Number, required: true }
 });
 
 const ProjectModel = mongoose.model('Project', ProjectSchema);
+
+app.use(cors());
+
+app.get('/names', async (req, res) => {
+    try {
+        const projects = await ProjectModel.find({});
+        res.json(projects);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Server is running on port 3000!');
+    verifyMySQLConnection();
+});
+
